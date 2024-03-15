7a736cc3662b5251d4d5992d5a4cc727370ee066
Zaclovespenguins <46504778+Zaclovespenguins@users.noreply.github.com>
2024-03-15 10:16:24 -0600

    add button toggle

--- app/config/Storage.js
+++ app/config/Storage.js
@@ -1,3 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Storage from 'react-native-storage';
+
 export const ShiniesCaught = new Storage({
   size: 2500,
   storageBackend: AsyncStorage,
--- app/screens/ListView.js
+++ app/screens/ListView.js
@@ -5,10 +5,9 @@ import {
     TouchableOpacity,
     View,
     Image,
-    Pressable,
-    Asy
+    Pressable
 } from 'react-native';
-import React, {useEffect, useContext} from "react";
+import React, {useEffect, useContext, useState} from "react";
 import {LinearGradient} from 'expo-linear-gradient';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
@@ -18,77 +17,174 @@ import {DexData} from "../config/Data";
 import {SearchTermContext} from "../config/Context";
 import {ShiniesCaught, AllCaught} from "../config/Storage";
 
+DexData.push ({
+    "index": 1027,
+    "ID": "1027",
+    "name": "fakemon",
+    "ability1": "gag reflex",
+    "ability2": "none",
+    "type1": "electric",
+    "type2": "dragon",
+    "descriptions": "{}",
+    "img": require('../../assets/DexData/PokeImages/1021-raging-bolt.png')
+},)
+
+for (let i = 1000; i < DexData.length; i++) {
+    console.log(DexData[i]);
+}
+
+
 export default function ListView({navigation: {navigate}}) {
     const SearchTerm = useContext(SearchTermContext);
 
-    async function toggleShiny(dexNumber) {
-        try {
-            let value = await ShiniesCaught.getItem('shiny');
-            if (value != null){
-                ShiniesCaught.remove({
-                    key: 'shiny',
-                    id: dexNumber
-                });
-            } else {
-                ShiniesCaught.save({
-                    key:'shiny',
-                    id: dexNumber,
-                    data:{
-                        dateTime: new Date()
-                    }
-                })}
-        } catch {
-            (e) => console.log(e)
-        }
+    // function toggleShiny(dexNumber) {
+    //     console.log(`Save ${dexNumber}`)
+    //
+    //     ShiniesCaught.save ({
+    //         key: 'shiny',
+    //         id: dexNumber,
+    //         data: {
+    //             dateTime: new Date()
+    //         },
+    //         expires: null
+    // })
+    // }
 
-    }
 
     function addObtained(dexNumber) {
         AllCaught.save({
-            key:dexNumber,
-            data:{
+            key: dexNumber,
+            data: {
                 dateTime: new Date()
             }
         })
     }
 
-    const Entry = ({item}) => (
-        <View style={{flex: 1, padding: 3,}}>
-            <TouchableOpacity
-                onPress={() => navigate('DetailView', {indexNumber: `${item.index}`, title: properName(item.name)})
-                }>
-                <LinearGradient colors={typeGradients[`${item['type1']}`]}
-                                start={[x = 1, y = 1]}
-                                end={[x = 0, y = 1]}
-                                style={styles.item}>
-                    <Text style={styles.title}>{properName(item.name)}</Text>
-                    <Image source={item.img} style={{flex: 1, resizeMode: 'contain', height: 100}}/>
-                    <Pressable
-                        onPress={() => addShiny(item.index)}
-                        style={({pressed}) => [
-                            {
-                                backgroundColor: '',
-                            },]}>{({pressed}) => (
-                        <Ionicons name={pressed ? "sparkles" : "sparkles-outline"} size={32} backgroundColor=''
-                                  color={pressed ? 'gold' : 'black'}/>
-                    )}
+    async function shinyCaughtCheck(dexNumber) {
+        var iconColor
+        var iconName
+        return await ShiniesCaught.load({key: 'shiny', id: dexNumber})
+            .then((c) => {
+                iconColor = 'gold'
+                iconName = 'sparkles'
+                return {iconColor, iconName}
+            })
+            .catch((e) => {
+                iconColor = 'black'
+                iconName = 'sparkles-outline'
+                return {iconColor, iconName}
+            });
+    }
 
-                    </Pressable>
-                    <Pressable
-                        onPress={() => console.log('pressed')}
-                        style={({pressed}) => [
-                            {
-                                backgroundColor: '',
-                            },]}>{({pressed}) => (
-                        <Ionicons name={pressed ? "checkmark-circle" : "checkmark-circle-outline"} size={32}
-                                  backgroundColor='' color={pressed ? 'green' : 'black'}/>
-                    )}
+    function Entry({item}) {
+        const [shinyStyleButtonColor, setShinyStyleButtonColor] = useState('black')
+        const [shinyStyleButtonName, setShinyStyleButtonName] = useState('sparkles-outline')
 
-                    </Pressable>
-                </LinearGradient>
-            </TouchableOpacity>
-        </View>
-    );
+        async function toggleShiny(dexNumber) {
+            await ShiniesCaught.load({key: 'shiny', id: dexNumber})
+                .then(ret => {
+                    setShinyStyleButtonColor('black')
+                    setShinyStyleButtonName('sparkles-outline')
+                    console.log(`removing ${dexNumber}`);
+                    ShiniesCaught.remove({
+                        key: 'shiny',
+                        id: dexNumber
+                    })
+                })
+                .catch((e) => {
+                    if (e.name === 'NotFoundError') {
+                        setShinyStyleButtonColor('gold')
+                        setShinyStyleButtonName('sparkles')
+                        console.log(`adding ${dexNumber}`);
+                        ShiniesCaught.save({
+                            key: 'shiny',
+                            id: dexNumber,
+                            data: {
+                                dateTime: new Date()
+                            }
+                        })
+                    } else {
+                        console.log(e.name)
+                    }
+                });
+        }
+
+        return (
+            <View style={{flex: 1, padding: 3,}}>
+                <TouchableOpacity
+                    onPress={() => navigate('DetailView', {indexNumber: `${item.index}`, title: properName(item.name)})
+                    }>
+                    <LinearGradient colors={typeGradients[`${item['type1']}`]}
+                                    start={[x = 1, y = 1]}
+                                    end={[x = 0, y = 0]}
+                                    style={styles.item}>
+                        <Text style={styles.title}>{properName(item.name)}</Text>
+                        <Image source={item.img} style={{flex: 1, resizeMode: 'contain', height: 100}}/>
+                        <Pressable
+                            onPress={() => toggleShiny(item.index)}
+                            style={({pressed}) => [
+                                {
+                                    backgroundColor: '',
+                                },]}>{({pressed}) => (
+                            <Ionicons name={shinyStyleButtonName} size={32} backgroundColor=''
+                                      color={shinyStyleButtonColor}/>
+                        )}
+                        </Pressable>
+                        <Pressable
+                            onPress={() => console.log(shinyCaughtCheck(item.index))}
+                            style={({pressed}) => [
+                                {
+                                    backgroundColor: '',
+                                },]}>{({pressed}) => (
+                            <Ionicons name={pressed ? "checkmark-circle" : "checkmark-circle-outline"} size={32}
+                                      backgroundColor='' color={pressed ? 'green' : 'black'}/>
+                        )}
+
+                        </Pressable>
+                    </LinearGradient>
+                </TouchableOpacity>
+            </View>
+        )
+
+    };
+
+
+    // const Entry = ({item}, shinyCaughtColor, shinyCaughtIcon) => (
+    //     <View style={{flex: 1, padding: 3,}}>
+    //         <TouchableOpacity
+    //             onPress={() => navigate('DetailView', {indexNumber: `${item.index}`, title: properName(item.name)})
+    //             }>
+    //             <LinearGradient colors={typeGradients[`${item['type1']}`]}
+    //                             start={[x = 1, y = 1]}
+    //                             end={[x = 0, y = 0]}
+    //                             style={styles.item}>
+    //                 <Text style={styles.title}>{properName(item.name)}</Text>
+    //                 <Image source={item.img} style={{flex: 1, resizeMode: 'contain', height: 100}}/>
+    //                 <Pressable
+    //                     onPress={() => toggleShiny(item.index)}
+    //                     style={({pressed}) => [
+    //                         {
+    //                             backgroundColor: '',
+    //                         },]}>{({pressed}) => (
+    //                     <Ionicons name="sparkles-outline" size={32} backgroundColor=''
+    //                               color="black"/>
+    //                 )}
+    //                 </Pressable>
+    //                 <Pressable
+    //                     onPress={() => console.log(shinyCaughtCheck(item.index))}
+    //                     style={({pressed}) => [
+    //                         {
+    //                             backgroundColor: '',
+    //                         },]}>{({pressed}) => (
+    //                     <Ionicons name={pressed ? "checkmark-circle" : "checkmark-circle-outline"} size={32}
+    //                               backgroundColor='' color={pressed ? 'green' : 'black'}/>
+    //                 )}
+    //
+    //                 </Pressable>
+    //             </LinearGradient>
+    //         </TouchableOpacity>
+    //     </View>
+    // );
 
     const filteredData = () => {
         return (
@@ -101,7 +197,7 @@ export default function ListView({navigation: {navigate}}) {
         <FlatList
             data={filteredData()}
             renderItem={({item}) => <Entry item={item}/>}
-            keyExtractor={item => item.index}
+            keyExtractor={item => item.name}
             contentInsetAdjustmentBehavior="automatic"
         />
     )
